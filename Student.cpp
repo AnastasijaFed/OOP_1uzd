c829039f1c827761cedf5aadd9391c1a0c5a5f93
AnastasijaFed <nastefedorenko@gmail.com>
2025-02-08 20:01:26 +0200

    pridetas meniu ir pazymiu generavimas

--- Student.cpp
+++ Student.cpp
@@ -59,13 +59,13 @@ double median(const Student& student) {
         return 0.0;
     }
 
-    std::vector<double> sorted_grades = student.grades; // Create a copy
-    std::sort(sorted_grades.begin(), sorted_grades.end()); // Sort the copy
+    std::vector<double> sorted_grades = student.grades;
+    std::sort(sorted_grades.begin(), sorted_grades.end());
 
     size_t middle = sorted_grades.size() / 2;
 
     if (sorted_grades.size() % 2 == 0) {
-        return (sorted_grades[middle - 1] + sorted_grades[middle]) / 2.0; // Correct, 2.0!
+        return (sorted_grades[middle - 1] + sorted_grades[middle]) / 2.0;
     } else {
         return sorted_grades[middle];
     }
@@ -95,11 +95,68 @@ void printStudentList(const vector<Student>& students) {
              << fixed << setprecision(2) << right << setw(15) << medFinal << endl;
     }
 }
+void generateGrades(vector<Student>& students) {
+    static bool seeded = false;
+    if (!seeded) {
+        srand(time(0));
+        seeded = true;
+    }
+
+    for (auto& student : students) {
+        int grades_number = rand() % 15 + 1;
+        student.grades.clear();
+        for (int i = 0; i < grades_number; ++i) {
+            student.grades.push_back(rand() % 11);
+        }
+        student.exam_grade = rand() % 11;
+    }
+}
 
 int main() {
     vector<Student> students;
-    students = addStudents(students);
-    printStudentList(students);
+    int menu_choice;
+    do {
+        cout << "\nPasirinkite programos eigą:\n";
+        cout << "1 - Įvesti studentų duomenis ranka\n";
+        cout << "2 - Generuoti pažymius\n";
+        cout << "3 - Generuoti studentų vardus, pavardes ir pažymius\n";
+        cout << "4 - Baigti darbą\n";
+        cout << "Jūsų pasirinkimas: ";
+        cin >> menu_choice;
+
+        switch (menu_choice) {
+            case 1:
+                students = addStudents(students);
+            break;
+            case 2:
+                if (students.empty()) {
+                    cout << "Pirmiausia sugeneruokite studentus arba įveskite ranka.\n";
+                } else {
+                    generateGrades(students);
+                }
+            break;
+            case 3:
+                int count;
+            cout << "Kiek studentų generuoti? ";
+            cin >> count;
+            //students = generateStudents(count);
+            generateGrades(students);
+            break;
+            case 4:
+                cout << "Programa baigė darbą.\n";
+            break;
+            default:
+                cout << "Neteisingas pasirinkimas. Bandykite dar kartą.\n";
+        }
+
+        if (menu_choice >= 1 && menu_choice <= 3) {
+            printStudentList(students);
+        }
+
+    } while (menu_choice != 4);
+
+
+
 
 
 
