35fb4ed9e6b82dac25aadad9851e1355411d0313
AnastasijaFed <nastefedorenko@gmail.com>
2025-02-09 13:41:46 +0200

    prideta struktura, kad galetu buti naudijamas C masyvas

---
+++ Student2.h
@@ -0,0 +1,18 @@
+//
+// Created by Anastasija Fedorenko on 2025-02-09.
+//
+
+#ifndef STUDENT2_H
+#define STUDENT2_H
+
+struct Student2 {
+  char name[20];
+  char surname[20];
+  int grades[50];
+  int exam_grade;
+  int num_grades;
+};
+
+
+
+#endif //STUDENT2_H
