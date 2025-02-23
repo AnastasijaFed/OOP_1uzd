2ff706a68bfa7360c72cdc70f806ad2673b1029d
AnastasijaFed <nastefedorenko@gmail.com>
2025-02-23 11:17:19 +0200

    prideta funkcija nuskaitymui is failo

--- Student.cpp
+++ Student.cpp
@@ -178,6 +178,40 @@ vector<Student> generateRandomStudents(int count, const std::string& first_names
         return students;
     }
 }
+vector<Student> readStudentsFile(const string& filename) {
+    vector<Student> students;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        throw runtime_error("Nepavyko atidaryti failo.");
+    }
+    else {
+        string header, line;
+        getline(file, header);
+
+
+        while (getline(file, line)) {
+            Student student;
+            std::istringstream iss(line);
+            iss >> student.name >> student.surname;
+            double grade;
+            while (iss >> grade) {
+                student.grades.push_back(grade);
+            }
+
+            student.exam_grade = static_cast<int>(student.grades.back());
+            student.grades.pop_back();
+
+            students.push_back(student);
+        }
+
+        file.close();
+
+    }
+
+    return students;
+}
+
+
 bool compareByName(const Student a, const Student b) {
     return a.name < b.name;
 }
