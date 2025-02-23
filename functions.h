0933c3f62285851a5f674c74f51322f456ca0762
AnastasijaFed <nastefedorenko@gmail.com>
2025-02-23 11:50:40 +0200

    funkcijos perkeltos i atskira faila

--- functions.h
+++ functions.h
@@ -5,11 +5,31 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include "Student.h"
+#include <fstream>
+#include <sstream>
+using namespace std;
+using namespace std::chrono;
 
-
-class functions {
-
-};
+vector<Student> addStudents(vector<Student> students);
+double average(const Student &student);
+double median(const Student &student);
+double calculateFinalGradesMedian(const Student &student);
+double calculateFinalGradesAverage(const Student &student);
+void printStudentList(const vector<Student> students);
+void generateGrades(vector<Student> &students);
+vector<string> loadFromFile(const string &fileName);
+vector<Student> readStudentsFile(const string &fileName);
+vector<Student> generateRandomStudents(int count, const std::string &first_names_file, const std::string &last_names_file);
+vector<Student> test();
+bool compareByName(const Student a, const Student b);
+bool compareBySurname(const Student a, const Student b);
+bool compareByAverage(const Student a, const Student b);
+bool compareByMedian(const Student a, const Student b);
+vector<Student> sortByName(vector<Student> students);
+vector<Student> sortBySurname(vector<Student> students);
+vector<Student> sortByAverage(vector<Student> students);
+vector<Student> sortByMedian(vector<Student> students);
 
 
 
